dd05bf5b08b0ded79254d7701024dff13bea0f84
刘爽 <liushuang@liushuangdemacbook-pro.local>
2016-10-26 15:08:29 +0800

    Initial Commit

---
+++ DSGBLEManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DSGBLEManager
+//
+//  Created by 刘爽 on 16/10/26.
+//  Copyright © 2016年 刘爽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DSGBLEManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DSGBLEManager
+//
+//  Created by 刘爽 on 16/10/26.
+//  Copyright © 2016年 刘爽. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
